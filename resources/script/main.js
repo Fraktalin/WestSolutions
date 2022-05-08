50b4f35cd997314b412d226420e583dfcef07678
Dmitry Shovkoplias <dmitryshovk@gmail.com>
2022-05-08 14:50:48 +0300

    added script for offer items

--- resources/script/main.js
+++ resources/script/main.js
@@ -1,4 +1,5 @@
 const selectOptions = document.querySelectorAll(".dropdown-item");
+const block = document.querySelector(".offer-block-items");
 let dataItems;
 for (const i of selectOptions) {
   i.addEventListener("click", setSelect);
@@ -23,4 +24,36 @@ readTextFile("./resources/script/data.json", function (text) {
   drowCards(dataItems);
 });
 
-function drowCards(data) {}
+function drowCards(data) {
+  data.forEach((item, indx) => {
+    let shopItem = `
+    <a class="offer-block-item col-sm-3">
+      <div class="offer-block-wrap">
+        <div class="image-item-wrap ${item.new ? `new` : ``} ${
+      item.sold ? `sold` : ""
+    }">
+          <img
+            src="${item.image}"
+            alt="car ${item.name}"
+            class="offer-block-image"
+          />
+        </div>
+        <div class="info-item-wrap">
+          <div class="top-item-info">
+            <span class="year-item">${item.year}</span>
+            <h4 class="name-item">${item.name}</h4>
+          </div>
+          <div class="mid-item-info">
+            <span class="feature-desc-item">${item.mileage.toLocaleString(
+              "en-IN"
+            )} km |</span>
+            <span class="feature-desc-item"> ${item.transmission} |</span>
+            <span class="feature-desc-item"> ${item.engine}</span>
+          </div>
+          <span class="price-item">$${item.price.toLocaleString("en-IN")}</span>
+        </div>
+      </div>
+    </a>`;
+    block.innerHTML += shopItem;
+  });
+}
