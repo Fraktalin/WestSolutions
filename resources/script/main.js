5f80990a7e8ea2a5ab5086359c944fb8e6478cc8
Dmitry Shovkoplias <dmitryshovk@gmail.com>
2022-05-08 13:22:00 +0300

    added offer cards

--- resources/script/main.js
+++ resources/script/main.js
@@ -1,8 +1,26 @@
 const selectOptions = document.querySelectorAll(".dropdown-item");
-
+let dataItems;
 for (const i of selectOptions) {
   i.addEventListener("click", setSelect);
 }
 function setSelect() {
   this.parentNode.parentNode.firstElementChild.innerText = this.innerText;
 }
+function readTextFile(file, callback) {
+  let rawFile = new XMLHttpRequest();
+  rawFile.overrideMimeType("application/json");
+  rawFile.open("GET", file, true);
+  rawFile.onreadystatechange = function () {
+    if (rawFile.readyState === 4 && rawFile.status == "200") {
+      callback(rawFile.responseText);
+    }
+  };
+  rawFile.send(null);
+}
+
+readTextFile("./resources/script/data.json", function (text) {
+  dataItems = JSON.parse(text);
+  drowCards(dataItems);
+});
+
+function drowCards(data) {}
